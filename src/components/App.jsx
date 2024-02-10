2a2053891578973c210187e7ff2b18814d3110bb
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-02-10 09:28:41 +0200

    added PhoneBook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { getUniqueId } from 'helpers/helpers';
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    phones: [],
+    name: '',
+  };
+  addContact = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => {
+      return {
+        phones: [
+          ...prevState.phones,
+          {
+            name: e.target.elements.name,
+            id: getUniqueId(),
+          },
+        ],
+        name: '',
+      };
+    });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <PhoneBook onSubmit={this.addContact} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class PhoneBook extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <button>Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default PhoneBook;
